d7ec0c8210ff2dc64225b30f445169f16ddb70fe
JoaoMatheus46 <joao.m.ribeiro@edu.senai.br>
2026-08-28 08:32:29 -0300

    Adicionar arquivos de projeto.

---
+++ Carro.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Abstracao
+{
+    internal class Carro
+    {
+        //ATRIBUTOS (CARACTERISTICAS)
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public string Cor;
+
+        //METODOS (CORPORTAMENTO)
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Marca: {Marca}, {Modelo}");
+            Console.WriteLine($"Cor: {Cor}, Ano: {Ano}");
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Abstracao
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidadeemestoque;
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Preço: {Preco}");
+            Console.WriteLine($"Quantidade em estoque: {Quantidadeemestoque}");
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
